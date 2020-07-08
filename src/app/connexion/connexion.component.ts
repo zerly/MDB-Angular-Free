667757966dab2a94198f2a3f9b1f1a9213bee48a
zerly <expedigame@gmail.com>
2020-07-08 02:33:40 +0200

    test

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -13,7 +13,7 @@ export class ConnexionComponent implements OnInit {
   constructor(private _httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    console.log('test');
+    console.log('tac');
     console.log(this.route.snapshot.queryParamMap.get('code'));
     var code = this.route.snapshot.queryParamMap.get('code');
     const req = this._httpClient.post('https://id.twitch.tv/oauth2/token?client_id=xn3g12yzv87p0ohpnfsi6o1rh638dm&client_secret=j7pnk23rmd6o630xkhh6ompbkig5n6&code='+ code +'&grant_type=authorization_code&redirect_uri=http://localhost:4200/twitch/login', {
